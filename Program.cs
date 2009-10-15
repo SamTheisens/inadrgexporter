f5811fa39304c7b540d136e845912b47d56da67f
sam.theisens <sam.theisens@92cd6b4c-b7c8-11de-97ce-e783fd196cab>
2009-10-15 09:19:11 +0000

    linked up input and output files to textboxes. Renamed Form1 to ExporterForm

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace INADRGExporter
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ExporterForm());
         }
     }
 }
