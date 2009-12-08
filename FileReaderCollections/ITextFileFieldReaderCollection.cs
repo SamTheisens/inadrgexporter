9e08842004389529b6ce9fadefa087d884d8b8c5
sam.theisens <sam.theisens@92cd6b4c-b7c8-11de-97ce-e783fd196cab>
2009-12-08 02:39:51 +0000

    Renamed FileReaders to FileReaderCollections

---
+++ FileReaderCollections/ITextFileFieldReaderCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace InadrgExporter.FileReaderCollections
+{
+    public interface ITextFileFieldReaderCollection : IEnumerable<Dictionary<string,string>>, IEnumerator<Dictionary<string,string>>
+    {
+        long Rows { get; }
+        void MoveToRow(int rowNumber);
+        string[] Headers { get; }
+    }
+}
