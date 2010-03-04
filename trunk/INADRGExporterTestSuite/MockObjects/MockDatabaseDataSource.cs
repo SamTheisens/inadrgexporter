f7c41c3793bf28caa25f00301ebf23631bc1bbe7
sam.theisens <sam.theisens@92cd6b4c-b7c8-11de-97ce-e783fd196cab>
2010-03-04 08:19:00 +0000

    Started work on unittests.

---
+++ trunk/INADRGExporterTestSuite/MockObjects/MockDatabaseDataSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using InadrgExporter.DataSources;
+
+namespace INADRGExporterTestSuite.MockObjects
+{
+    class MockDatabaseDataSource : DatabaseDataSource
+    {
+        public MockDatabaseDataSource(DateTime from, DateTime until, string kdCustomer, DataTable table) : base(from, until, kdCustomer, table)
+        {
+        }
+    }
+}
